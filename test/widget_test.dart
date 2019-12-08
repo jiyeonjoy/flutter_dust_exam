61cc1d8ce54174afe9c0d3f8768b69a4f5891bec
jiyeonjoy <choi02647@naver.com>
2019-12-09 00:39:05 +0900

    Json 파일을 Dart 로 변환하여 테스트

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,10 +6,12 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dust/models/air_result.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_dust/main.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 // 데이터를 잘 가져오는지 확인하는 테스트 코드.
 // 옆에 초록색 삼각형 클릭하면 확인 가능!!
@@ -18,5 +20,8 @@ void main() {
     var response = await http.get('https://api.airvisual.com/v2/nearest_city?key=1164b7b9-e98f-4971-8a48-94a31ada783a');
 
     expect(response.statusCode, 200);
+
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    expect(result.status, 'success');
   });
 }
