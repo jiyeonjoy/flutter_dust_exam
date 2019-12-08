7ea78e3bcc6823143bcd8c39477e7b9884d48118
jiyeonjoy <choi02647@naver.com>
2019-12-09 00:10:50 +0900

    레이아웃 완성

--- lib/main.dart
+++ lib/main.dart
@@ -10,85 +10,94 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Main(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-
-  final String title;
-
+class Main extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainState createState() => _MainState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              '현재 위치 미세먼지',
+              style: TextStyle(fontSize: 30),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            // 공간 생성하는 것임. 패딩과 같은 것 임
+            SizedBox(
+              height: 16,
             ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  // Row 에는 배경색 지정 못해서 Container로 감싼것임
+                  // Container 는 왠만하면 모든 속성 다 있음
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    color: Colors.yellow,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text('얼굴사진'),
+                        Text(
+                          '80',
+                          style: TextStyle(fontSize: 40),
+                        ),
+                        Text('보통', style: TextStyle(fontSize: 20))
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text('이미지'),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Text('11º', style: TextStyle(fontSize: 16))
+                          ],
+                        ),
+                        Text('습도 100%'),
+                        Text('풍속 5.7m/s')
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            // 모서리 둥글레 해주는 위젯
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: RaisedButton(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15.0, horizontal: 50),
+                color: Colors.orange,
+                child: Icon(
+                  Icons.refresh,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
