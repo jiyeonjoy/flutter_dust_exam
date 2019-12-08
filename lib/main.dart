a3b6726850c996e4761a2721c936791f319a2d19
jiyeonjoy <choi02647@naver.com>
2019-12-09 01:11:29 +0900

    실제 미세먼지 데이터 표시하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dust/models/air_result.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -21,83 +24,149 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
+  // json 값 모델
+  AirResult _result;
+
+  // AirResult 사이트에서 정보값 가져오게 하는 비동기 메소드
+  Future<AirResult> fetchData() async {
+    var response = await http.get(
+        'https://api.airvisual.com/v2/nearest_city?key=1164b7b9-e98f-4971-8a48-94a31ada783a');
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    return result;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // 초기화시 비동기 시작하여 끝나면 정보를 가져와서 다시 화면 그려질 수 있게 한다.
+    fetchData().then((airResult) {
+      setState(() {
+        _result = airResult;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // 비동기로 미세먼지 정보를 받아오다 보니 build가 먼저 실행되면 에러뜸
+      // 따라서 아직 _result 값이 널이면 프로그래스바가 돌면서 기다리게 설정해줌!!!
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '현재 위치 미세먼지',
-              style: TextStyle(fontSize: 30),
-            ),
-            // 공간 생성하는 것임. 패딩과 같은 것 임
-            SizedBox(
-              height: 16,
-            ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  // Row 에는 배경색 지정 못해서 Container로 감싼것임
-                  // Container 는 왠만하면 모든 속성 다 있음
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                    color: Colors.yellow,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Text('얼굴사진'),
-                        Text(
-                          '80',
-                          style: TextStyle(fontSize: 40),
-                        ),
-                        Text('보통', style: TextStyle(fontSize: 20))
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Row(
+        child: _result == null
+            ? CircularProgressIndicator()
+            : Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '현재 위치 미세먼지',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      // 공간 생성하는 것임. 패딩과 같은 것 임
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Card(
+                        child: Column(
                           children: <Widget>[
-                            Text('이미지'),
-                            SizedBox(
-                              width: 16,
+                            // Row 에는 배경색 지정 못해서 Container로 감싼것임
+                            // Container 는 왠만하면 모든 속성 다 있음
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                              color: _getColor(_result),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: <Widget>[
+                                  Text('얼굴사진'),
+                                  Text(
+                                    '${_result.data.current.pollution.aqicn}',
+                                    style: TextStyle(fontSize: 40),
+                                  ),
+                                  Text(_getString(_result),
+                                      style: TextStyle(fontSize: 20))
+                                ],
+                              ),
                             ),
-                            Text('11º', style: TextStyle(fontSize: 16))
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Image.network(
+                                        'https://airvisual.com/images/03d.png',
+                                        width: 32,
+                                        height: 32,
+                                      ),
+                                      SizedBox(
+                                        width: 16,
+                                      ),
+                                      Text(
+                                          '${_result.data.current.weather.tp}º',
+                                          style: TextStyle(fontSize: 16))
+                                    ],
+                                  ),
+                                  Text(
+                                      '습도 ${_result.data.current.weather.hu}%'),
+                                  Text(
+                                      '풍속 ${_result.data.current.weather.ws}m/s')
+                                ],
+                              ),
+                            ),
                           ],
                         ),
-                        Text('습도 100%'),
-                        Text('풍속 5.7m/s')
-                      ],
-                    ),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      // 모서리 둥글레 해주는 위젯
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: RaisedButton(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 15.0, horizontal: 50),
+                          color: Colors.orange,
+                          child: Icon(
+                            Icons.refresh,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
                   ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            // 모서리 둥글레 해주는 위젯
-            ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              child: RaisedButton(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15.0, horizontal: 50),
-                color: Colors.orange,
-                child: Icon(
-                  Icons.refresh,
-                  color: Colors.white,
                 ),
-                onPressed: () {},
               ),
-            )
-          ],
-        ),
       ),
     );
+  }
+
+  Color _getColor(AirResult result) {
+    if (result.data.current.pollution.aqicn <= 50) {
+      return Colors.greenAccent;
+    } else if (result.data.current.pollution.aqicn <= 100) {
+      return Colors.yellow;
+    } else if (result.data.current.pollution.aqicn <= 150) {
+      return Colors.orange;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  String _getString(AirResult result) {
+    if (result.data.current.pollution.aqicn <= 50) {
+      return '좋음';
+    } else if (result.data.current.pollution.aqicn <= 100) {
+      return '보통';
+    } else if (result.data.current.pollution.aqicn <= 150) {
+      return '나쁨';
+    } else {
+      return '최악';
+    }
   }
 }
